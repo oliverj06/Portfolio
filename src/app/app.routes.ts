65431cd8af5a8631506228d79072a86837cfc27e
Oliver Juliano <oliverdj0615@gmail.com>
2026-03-24 14:39:16 -0700

    Use Hash Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-
 
     { path: '', component: MainLayout, children: [
         {path: '', component: Home, title: "Oliver Juliano"}
     ]}
 
 ];
+
+RouterModule.forRoot(routes, {useHash: true});
+
