4ebb76371f6e0b4c92e8792886631327d0427e6b
Oliver Juliano <oliverdj0615@gmail.com>
2026-03-24 14:33:41 -0700

    Added Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    { path: '', component: MainLayout, children: [
+        {path: '', component: Home, title: "Oliver Juliano"}
+    ]}
+
+];
---
+++ src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../components/navbar/navbar";
+import { RouterOutlet } from '@angular/router';
+import { Footer } from "../../components/footer/footer";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [Navbar, RouterOutlet, Footer],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+})
+export class MainLayout {}
